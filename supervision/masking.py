23594c5553fbb457e366fba601dc16c757477425
Spiros Thermos <spthermo@e-ce.uth.gr>
2019-09-04 14:57:09 +0300

    Delete masking.py

--- supervision/masking.py
+++
@@ -1,6 +0,0 @@
-import torch
-
-def get_mask(depth, min_threshold=0.1, max_threshold=6.5):    
-    mask = ((depth < max_threshold) & (depth > min_threshold)).detach().type(depth.dtype)
-    count = torch.sum(mask).item()
-    return mask, count
